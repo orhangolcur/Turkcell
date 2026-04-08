f53cc9660e1233164d6a038a1510448e3e2bf98e
Orhan Gölcür <orhangolcur0@gmail.com>
2026-04-08 20:48:40 +0300

    body kısmında json formatında veri iletildi

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.turkcell.spring_starter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.turkcell.spring_starter.model.Product;
 
 @RestController // Bu class bir rest contollerdır, içini uygulama başladığında tara, http=> function tanımlarını al
 @RequestMapping("/api/product") // http://localhost:8080/api/product => sayHi() çalışır
@@ -19,4 +23,9 @@ public class ProductController {
     public String sayHello(@PathVariable String name, @PathVariable int age) {
         return "Hello, " + name + " yaşınız: " + age;
     } 
+
+    @PostMapping // http://localhost:8080/api/product => add() çalışır, body kısmına json formatında product bilgisi gönderilir
+    public String add(@RequestBody Product product) { // Json => Java objesine
+        return product.getId() + " ID'li ürün başarıyla eklendi, fiyatı: " + product.getPrice();
+    }
 }
