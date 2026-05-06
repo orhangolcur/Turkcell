3682e6605dc9dabbe90240923274585ab8ade78a
Orhan Gölcür <orhangolcur0@gmail.com>
2026-05-06 19:47:28 +0300

    jwt eklendi

---
+++ spring-cqrs/src/main/java/com/turkcell/spring_cqrs/application/features/user/rule/UserBusinessRules.java
@@ -0,0 +1,13 @@
+package com.turkcell.spring_cqrs.application.features.user.rule;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBusinessRules {
+
+    public void userWithSameEmailMustNotExist(String email)
+    {
+        //...
+    }
+
+}
---
+++ spring-cqrs/src/main/java/com/turkcell/spring_cqrs/web/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.turkcell.spring_cqrs.web.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.turkcell.spring_cqrs.application.features.user.command.register.RegisterCommand;
+import com.turkcell.spring_cqrs.application.features.user.command.register.RegisterResponse;
+import com.turkcell.spring_cqrs.core.mediator.Mediator;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final Mediator mediator;
+
+    public AuthController(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @PostMapping("register")
+    public RegisterResponse register(@RequestBody @Valid RegisterCommand command)
+    {
+        return mediator.send(command);
+    }
+    
+}
